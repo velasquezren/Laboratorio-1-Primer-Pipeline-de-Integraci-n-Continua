6304bfa65a6eeb5faf7ff49fe1f29204e1d7347f
velasquezren <velasquez.rene@ficct.uagrm.edu.bo>
2026-08-18 18:26:22 -0400

    test: simular fallo deliberado en pruebas unitarias para evaluar Quality Gate

--- app/calculator.py
+++ app/calculator.py
@@ -3,8 +3,8 @@ Módulo de operaciones de la aplicación para Laboratorio 3 CI/CD.
 """
 
 def add(a: float, b: float) -> float:
-    """Retorna la suma de dos números."""
-    return a + b
+    """Retorna la suma de dos números (MODIFICADO DELIBERADAMENTE PARA PROVOCAR FALLO)."""
+    return a + b + 999
 
 def subtract(a: float, b: float) -> float:
     """Retorna la resta de dos números."""
